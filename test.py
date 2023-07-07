811de00c865940d179b54008c204dd1d35e2366b
Patel Anil <patelanil1441@gmail.com>
2023-07-07 13:58:37 +0800

    Initial Pair Trading

---
+++ test.py
@@ -0,0 +1,15 @@
+from ast import Import
+
+
+import pandas as pd
+import numpy as np
+import math 
+#import seaborn as sns 
+import matplotlib.pyplot as plt
+#from sklearn import metrics
+#from sklearn.model_selection import train_test_split
+import yfinance as yf  # We will use this library to upload latest data from Yahoo API
+
+plt.style.use('fivethirtyeight')
+
+print ("All Imported Successfully")
